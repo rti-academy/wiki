152c03577ba763c7c7d259c4f2cb171ac1d03bdf
Efim Terentiev <klg.efim-terentiev@yandex.ru>
2019-08-14 13:00:19 +0300

    Add comments component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { CommentsService } from './services/comments.service';
 import { CommentsComponent } from './components/comments/comments.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -17,6 +20,9 @@ import { CommentsComponent } from './components/comments/comments.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatSidenavModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
   ],
   providers: [
     CommentsService,
--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CommentsService } from '@app/services/comments.service';
+import { Comment } from '@app/models/comment';
 
 @Component({
-  selector: 'app-comments',
-  templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.css']
+    selector: 'app-comments',
+    templateUrl: './comments.component.html',
+    styleUrls: ['./comments.component.css']
 })
 export class CommentsComponent implements OnInit {
+    public comments: Comment[];
 
-  constructor() { }
+    public commentText = '';
 
-  ngOnInit() {
-  }
+    @Input()
+    public articleId: number;
 
+    constructor(private commentService: CommentsService) {
+    }
+
+    ngOnInit() {
+        this.comments = this.commentService.getCommentsByArticleId(this.articleId);
+    }
+
+    addComment() {
+
+        this.commentService.add({
+            articleId: this.articleId,
+            text: this.commentText
+        });
+
+        this.comments = this.commentService.getCommentsByArticleId(this.articleId);
+
+        this.resetCommentField();
+    }
+
+    private resetCommentField() {
+        this.commentText = '';
+    }
+
+    public getCommentCaption() {
+        const variants: string[] = ['комментарий', 'комментария', 'комментариев'];
+        const lastTenDigits = Math.abs(this.comments.length) % 100;
+        const lastDigit = lastTenDigits % 10;
+        if (lastTenDigits > 10 && lastTenDigits < 20) { return variants[2]; }
+        if (lastDigit > 1 && lastDigit < 5) { return variants[1]; }
+        if (lastDigit === 1) { return variants[0]; }
+        return variants[2];
+    }
 }
