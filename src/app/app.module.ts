3b13a89c625b7cda81aa88622700bccac12eacfb
Dmitriy Kotov <yadmitry1@mail.ru>
2019-08-14 13:07:53 +0300

    Configure articles routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { ArticleComponent } from "./components/article/article.component";
 
 @NgModule({
   declarations: [AppComponent, ArticleComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatSidenavModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    RouterModule.forRoot([
+      { path: "", component: ArticleComponent },
+      { path: "articles/:id", component: ArticleComponent }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,14 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Article } from "../../models/article";
-
-const mock_article = {
-  id: 2,
-  title: "Заголовок",
-  creationTime: new Date("2019-08-13"),
-  version: 1,
-  content: "Какой-то контент",
-  parentId: 1
-};
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-article",
@@ -16,22 +8,11 @@ const mock_article = {
   styleUrls: ["./article.component.css"]
 })
 export class ArticleComponent implements OnInit {
-  article: Article = mock_article;
-  creationTime: string = this.dateFormat(mock_article.creationTime);
+  article: Article;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit() {}
-
-  dateFormat(date: Date): string {
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDay();
-
-    let newMonth;
-    if (month.toString.length == 1) {
-      newMonth = `0${month}`;
-    }
-    return `${day}.${newMonth}.${year}`;
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {});
   }
 }
