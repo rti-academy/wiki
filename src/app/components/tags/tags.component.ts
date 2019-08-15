cf352acbdb3bf2bac20837a46091d349b3dc5ba9
Efim Terentiev <klg.efim-terentiev@yandex.ru>
2019-08-15 12:47:53 +0300

    #4: Fix autocomplete

--- src/app/components/tags/tags.component.ts
+++ src/app/components/tags/tags.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Input } from '@angular/core';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { FormControl } from '@angular/forms';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
 import { Observable } from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
+import { map, startWith } from 'rxjs/operators';
 
 import { Tag } from '../../models/tag';
 import { TagService } from '../../services/tag.service';
@@ -20,62 +20,81 @@ export class TagsComponent implements OnInit {
   removable = true;
   separatorKeysCodes: number[] = [ENTER, COMMA];
 
-  tags: Tag[] = [];
-  
+  allTags: Tag[];
+  articleTags: Tag[] = [];
+
   tagCtrl = new FormControl();
   filteredTags: Observable<Tag[]>;
 
-  @ViewChild('auto', {static: false}) matAutocomplete: MatAutocomplete;
-  @ViewChild('tagInput', {static: false}) tagInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto', { static: false })
+  matAutocomplete: MatAutocomplete;
 
-  constructor(private tagService: TagService) { }
-  
- add(event: MatChipInputEvent): void {
+  @ViewChild('tagInput', { static: false })
+  tagInput: ElementRef<HTMLInputElement>;
+
+  constructor(private tagService: TagService) {
+    this.allTags = tagService.getAll();
+  }
+
+  add(event: MatChipInputEvent): void {
     // Add tag only when MatAutocomplete is not open
     // To make sure this does not conflict with OptionSelected Event
-    if (!this.matAutocomplete.isOpen) {
-      const input = event.input;
-      const value = event.value;
+    const input = event.input;
+    const value = event.value;
+
+    // if (!this.matAutocomplete.isOpen && !this.isContainTag(value)) {
+    if (!this.isContainTag(value)) {
 
       // Add our tag
       if ((value || '').trim()) {
-        this.tags = this.tagService.add(value);
+        this.allTags = this.tagService.add(value);
+        const lastInsertedTag = this.allTags[this.allTags.length - 1];
+        this.articleTags.push(lastInsertedTag);
       }
 
       // Reset the input value
       if (input) {
         input.value = '';
+        this.tagCtrl.setValue('');
       }
-
-      this.tagCtrl.setValue('');
     }
   }
 
   remove(tag: Tag): void {
-    this.tags = this.tagService.remove(tag);
+    this.allTags = this.tagService.remove(tag); // удалять только из статьи
+    const index = this.articleTags.findIndex(t => t === tag);
+    this.articleTags.splice(index, 1);
   }
 
- selected(event: MatAutocompleteSelectedEvent): void {
-    console.log(event);
-   
-    /*this.tags.push({id:0, value: event.option.viewValue});
-    this.tagInput.nativeElement.value = '';
-    this.tagCtrl.setValue('');*/
+  selected(event: MatAutocompleteSelectedEvent): void {
+    if (!this.isContainTag(event.option.viewValue)) {
+      this.articleTags.push({ id: 0, value: event.option.viewValue });
+      this.tagInput.nativeElement.value = '';
+      this.tagCtrl.setValue('');
+    }
   }
-  
+
 
   ngOnInit() {
     this.filteredTags = this.tagCtrl.valueChanges
       .pipe(
-        map(value => this._filter(value))
+        map(value => {
+          return this._filter(value);
+        })
       );
-  };
+  }
+
+  private isContainTag(tagValue: string) {
+    // TODO
+    const index = this.articleTags.findIndex(t => t.value.toLowerCase() === tagValue.toLowerCase());
+    return index >= 0;
+  }
 
   private _filter(value: string): Tag[] {
     const filterValue = value.toLowerCase();
     return value
-      ? this.tags.filter(option => option.value.toLowerCase().includes(filterValue))
+      ? this.allTags.filter(option => option.value.toLowerCase().includes(filterValue))
       : [];
-  };
+  }
 
 }
--- src/app/services/tag.service.ts
+++ src/app/services/tag.service.ts
@@ -2,40 +2,41 @@ import { Injectable } from '@angular/core';
 import { TagsComponent } from '../components/tags/tags.component';
 import { Tag } from '../models/tag';
 
+const tagsMock = [
+  { id: 1, value: 'Home' },
+  { id: 2, value: 'Sun' },
+  { id: 3, value: 'Ball' }
+];
+
 @Injectable({
   providedIn: 'root'
 })
 export class TagService {
-  public allTag: Tag[] = [
-    {id: 1, value: "Home"},
-    {id: 2, value: "Sun"},
-    {id: 3, value: "Ball"}
-  ];
-  public tags: Tag[] = [];
-  public idCount: number = 0;
+  public tags: Tag[] = tagsMock;
+  public idCount = 0;
 
   constructor() { }
 
-  public add(value: string): Tag[] {  
-    this.idCount++; 
-    this.tags.push({ id: this.idCount, value: value });
-    return this.tags;    
+  public add(value: string): Tag[] {
+    this.idCount++;
+    this.tags.push({ id: this.idCount, value });
+    return this.tags;
   }
 
-  public remove(tag: Tag): Tag[]{
+  public remove(tag: Tag): Tag[] {
     const index = this.tags.indexOf(tag);
     if (index >= 0) {
       this.tags.splice(index, 1);
-    };
+    }
     return this.tags;
-    
-  };
+
+  }
 
   public getAll(): Tag[] {
     return this.tags;
   }
 
   public getById(id: number): Tag {
-      return this.tags.find(element => (element.id === id));
+    return this.tags.find(element => (element.id === id));
   }
 }
