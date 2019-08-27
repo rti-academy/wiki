92c8652a945ed1b421fcb33d07ed4335e9b02918
Sofia Piskareva <nimlot33@gmail.com>
2019-08-27 13:13:57 +0300

    #17: Add file download and delete

--- src/app/components/file-uploader/file-uploader.component.ts
+++ src/app/components/file-uploader/file-uploader.component.ts
@@ -1,42 +1,66 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { FileService } from '@app/services/file.service';
 import { AttachedFile } from '@app/models/attached-file';
+import { DeleteDialogComponent } from '../delete-dialog/delete-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-file-uploader',
   templateUrl: './file-uploader.component.html',
   styleUrls: ['./file-uploader.component.css']
 })
-export class FileUploaderComponent implements OnInit {
+export class FileUploaderComponent implements OnInit, OnChanges {
   public uploadForm: FormGroup;
   @Input()
   public articleId: number;
   public files: AttachedFile[] = [];
-  constructor(private formBuilder: FormBuilder, private fileService: FileService) { }
+
+  constructor(private formBuilder: FormBuilder,
+              private fileService: FileService,
+              private dialog: MatDialog) { }
 
   ngOnInit() {
-    this.fileService.getByArticleId(this.articleId).subscribe((result: any) => {
-      this.files = result.files;
-    })
     this.uploadForm = this.formBuilder.group({
       profile: ['']
     });
   }
-  onFileSelect(event) {
+  ngOnChanges() {
+    this.loadArticleFiles();
+  }
+  private loadArticleFiles() {
+    this.fileService.getByArticleId(this.articleId).subscribe((result: any) => {
+      this.files = result.files;
+    });
+  }
+  public onFileSelect(event) {
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
       this.uploadForm.get('profile').setValue(file);
       const formData = new FormData();
       formData.append('file', this.uploadForm.get('profile').value);
-      this.fileService.upload(formData, this.articleId).subscribe();
+      this.fileService.upload(formData, this.articleId).subscribe(() => this.loadArticleFiles());
     }
   }
+  public deleteFile(file: AttachedFile) {
+    this.fileService.delete(file.id, this.articleId).subscribe((response: any) =>
+    this.loadArticleFiles());
+  }
 
-  // onSubmit() {
-  //   const formData = new FormData();
-  //   formData.append('file', this.uploadForm.get('profile').value);
-  //   this.fileService.upload(formData, this.articleId).subscribe(response =>
-  //     console.log(response));
-  // }
+  public openDeleteDialog(file: AttachedFile): void {
+    const dialogRef = this.dialog.open(
+      DeleteDialogComponent,
+      {
+        width: '400px',
+        data: `файл ${file.name}`,
+      },
+    );
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.fileService.delete(file.id, this.articleId).subscribe((response: any) =>
+        this.loadArticleFiles());
+      }
+    });
+  }
 }
--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +10,21 @@ export class FileService {
   constructor(private http: HttpClient) { }
 
   public upload(file: FormData, articleId: number){
+    console.log(articleId);
     return this.http.post(`/api/article/${articleId}/file`, file);
   }
 
   public getByArticleId(articleId: number) {
     return this.http.get(`/api/article/${articleId}/file`);
   }
+
   public download(fileId: number, articleId: number) {
-    console.log(articleId);
-    console.log(fileId);
-    return this.http.get(`/api/article/${articleId}/file/${fileId}`);
+    return this.http.get(`/api/article/${articleId}/file/${fileId}`)
+    .pipe(
+      map((response: any) => console.log(response))
+    );
+  }
+  public delete(fileId: number, articleId: number) {
+    return this.http.delete(`/api/article/${articleId}/file/${fileId}`);
   }
 }
